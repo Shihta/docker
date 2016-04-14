432474c68505927b2681fecc253a5e2f15e52316
Shihta Kuan <kuster.die9841@gmail.com>
2016-04-14 15:53:50 +0800

    Add missing files

---
+++ images/tc/sync.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+DIRS="php/5.6/apache postgres/9.5 redis/3.0"
+ORIBASE="docker-library"
+if [ -f "/usr/local/opt/gnu-sed/libexec/gnubin/sed" ]; then
+    SED="/usr/local/opt/gnu-sed/libexec/gnubin/sed"
+else
+    SED="sed"
+fi
+
+for dir in $DIRS; do
+    cp -r ../$ORIBASE/$dir/* ./$dir
+    $SED -i 's/debian:jessie/shihta\/base-debian-jessie/g' ./$dir/Dockerfile
+done
