4ba86c2c76162aa4bfff542af3cef04f9ef8bd92
Shihta Kuan <kuster.die9841@gmail.com>
2017-05-31 12:07:13 +0800

    Add docker-nginx

--- dockerfiles/sync.sh
+++ dockerfiles/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 #DIRS="php/5.6/apache redis/3.2 mariadb/10.1 docker-nginx/stable/jessie python/2.7 docker-selenium/StandaloneChrome"
-DIRS="python/2.7"
+DIRS="python/2.7 docker-nginx/stable/stretch"
 ORIBASE="libraries"
 if [ -f "/usr/local/opt/gnu-sed/libexec/gnubin/sed" ]; then
     SED="/usr/local/opt/gnu-sed/libexec/gnubin/sed"
@@ -15,6 +15,7 @@ for dir in $DIRS; do
     cp -r ../$ORIBASE/$dir/* ./$dir
     $SED -i 's/debian:jessie/shihta\/buildpack-deps-jessie/g' ./$dir/Dockerfile
     $SED -i 's/buildpack-deps:jessie/shihta\/buildpack-deps-jessie/g' ./$dir/Dockerfile
+    $SED -i 's/debian:stretch.\+/shihta\/buildpack-deps-stretch/g' ./$dir/Dockerfile
     if [ -f "./$dir/Dockerfile.pad" ]; then
         cat "./$dir/Dockerfile.pad" >> "./$dir/Dockerfile"
     fi
