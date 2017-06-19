29654949732cb914c3ee590389f1e02ed27bf933
Shihta Kuan <kuster.die9841@gmail.com>
2017-06-19 15:20:40 +0800

    Add openjdk

--- dockerfiles/sync.sh
+++ dockerfiles/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 #DIRS="php/5.6/apache redis/3.2 mariadb/10.1 docker-nginx/stable/jessie python/2.7 docker-selenium/StandaloneChrome"
-DIRS="python/2.7 docker-nginx/mainline/stretch-perl"
+DIRS="python/2.7 docker-nginx/mainline/stretch-perl openjdk/8-jre"
 ORIBASE="libraries"
 if [ -f "/usr/local/opt/gnu-sed/libexec/gnubin/sed" ]; then
     SED="/usr/local/opt/gnu-sed/libexec/gnubin/sed"
@@ -13,6 +13,7 @@ fi
 
 for dir in $DIRS; do
     cp -r ../$ORIBASE/$dir/* ./$dir
+    $SED -i 's/buildpack-deps:jessie-curl/shihta\/buildpack-deps-jessie/g' ./$dir/Dockerfile
     $SED -i 's/debian:jessie/shihta\/buildpack-deps-jessie/g' ./$dir/Dockerfile
     $SED -i 's/buildpack-deps:jessie/shihta\/buildpack-deps-jessie/g' ./$dir/Dockerfile
     $SED -i 's/debian:stretch.\+/shihta\/buildpack-deps-stretch/g' ./$dir/Dockerfile
